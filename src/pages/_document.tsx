ed7bf1577776bc5bedbcd90aae72b45830aacb91
valtercioj <juniorvaltercio2000@gmail.com>
2023-03-02 15:43:38 -0300

    changing document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Icon from "public/icon.png";
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
+      <link rel="shortcut icon" href="/icon.png" />
+      <title>Netflix</title>
       <body className="bg-black">
         <Main />
         <NextScript />
